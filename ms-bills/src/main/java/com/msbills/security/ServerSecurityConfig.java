d6f783f925904cd806dcdcf02256240df9881f98
teffidev <tefyberal09@gmail.com>
2023-11-21 20:40:54 -0500

    first commit

---
+++ ms-bills/src/main/java/com/msbills/security/ServerSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.msbills.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ServerSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new JwtAuthConverter());
+
+        httpSecurity.authorizeHttpRequests()
+                .antMatchers("/bills/all/**").hasRole("user")
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
+
+        return httpSecurity.build();
+    }
+}
